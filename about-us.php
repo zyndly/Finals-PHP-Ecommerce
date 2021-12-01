e8169c6a28c65080ba5ffc4d5e639f3e5f620dd5
Zyndly <zyndlykentperlinbaja@gmail.com>
2021-12-01 11:12:22 +0800

    removed header

--- about-us.php
+++ about-us.php
@@ -1,3 +1,10 @@
+<?php
+require "config/constants.php";
+session_start();
+if(isset($_SESSION["uid"])){
+	header("location:profile.php");
+}
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -9,6 +16,8 @@
 		<script src="main.js"></script>
 		<link rel="stylesheet" type="text/css" href="style.css">
 
+		<link rel="stylesheet" type="text/css" href="style.css">
+
 		<link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
 		<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic,900,900italic&amp;subset=latin,latin-ext" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Open%20Sans:300,400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext" rel="stylesheet">
@@ -18,17 +27,14 @@
 		<link rel="stylesheet" type="text/css" href="assets/css/chosen.min.css">
 		<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 		<link rel="stylesheet" type="text/css" href="assets/css/color-01.css">
-		<!--
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
--->
+		<link rel="stylesheet" type="text/css" href="assets/css/policy.css">
+
 		<style></style>
 	</head>
-<!-- HERE -->
 <body>
 <div class="wait overlay">
 	<div class="loader"></div>
 </div>
-<!--NAVBAR-->
 	<div class="navbar navbar-inverse navbar-fixed-top">
 		<div class="container-fluid">	
 			<div class="navbar-header">
@@ -38,8 +44,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a href="index.php" class="navbar-brand">
-				Maxwell Shop                                     </a>
+				<a href="index.php" class="navbar-brand">MAXWELL</a>
 			</div>
 		<div class="collapse navbar-collapse" id="collapse">
 			<form class="navbar-form navbar-left">
@@ -48,89 +53,46 @@
 		        </div>
 		        <button type="submit" class="btn btn-primary" id="search_btn"><span class="glyphicon glyphicon-search"></span></button>
 		     </form>
-			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-shopping-cart"></span> Cart <span class="badge" >0</span></a>
-					<div class="dropdown-menu" style="width:500px;">
-						<div class="panel panel-success">
-							<div class="panel-heading">
-								<div class="row">
-									<div class="col-md-3">Sl.No</div>
-									<div class="col-md-3">Product Image</div>
-									<div class="col-md-3">Product Name</div>
-									<div class="col-md-3">Price in <?php echo CURRENCY; ?></div>
-								</div>
-							</div>
-							<div class="panel-body">
-								<div id="cart_product">
-								</div>
-							</div>
-							<div class="panel-footer"></div>
-						</div>
-					</div>
-				</li>
-		<!--END MARK -->
-				<li><a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> Login/Register</a>
-					<ul class="dropdown-menu">
-						<div style="width:300px;">
-							<div class="panel panel-primary">
-								<div class="panel-heading">Login</div>
-								<div class="panel-heading">
-									<form onsubmit="return false" id="login">
-										<label for="email">Email</label>
-										<input type="email" class="form-control" name="email" id="email" required/>
-										<label for="email">Password</label>
-										<input type="password" class="form-control" name="password" id="password" required/>
-										<p><br/></p>
-										<input type="submit" class="btn btn-warning" value="Login">
-										<a href="customer_registration.php?register=1" style="color:white; text-decoration:none;">Create Account Now</a>
-									</form>
-								</div>
-								<div class="panel-footer" id="e_msg"></div>
-							</div>
-						</div>
-					</ul>
-				</li>
-			</ul>
 		</div>
 	</div>
-	
-	<!--header-->
-	<header id="header" class="header header-style-1">
-		<div class="container-fluid">
-			<div class="row">
-				<div class="topbar-menu-area">
-					<div class="container">
-					</div>
-				</div>
-					</div>
-				</div>
-					<div class="primary-nav-section">
-						<div class="container">
-							<ul class="nav primary clone-main-menu" id="mercado_main" data-menuname="Main menu" >
-								<li class="menu-item home-icon">
-									<a href="index.php" class="link-term mercado-item-title"><i class="fa fa-home" aria-hidden="true"></i></a>
-								</li>
-								<li class="menu-item">
-									<a href="about-us.php" class="link-term mercado-item-title">About Us</a>
-								</li>
-								<li class="menu-item">
-									<a href="contact-us.php" class="link-term mercado-item-title">Contact Us</a>
-								</li>																	
-							</ul>
+</div>	
+<!--MID NAVBAR-->
+		<div class="mid-section main-info-area">
+		<div id="spacing">
+			<P><br></P>
+			</div>
+		<div id="header" class="header header-style-1">
+					<div class="container-fluid">
+						<div class="row">
+							<div class="topbar-menu-area">
+								<div class="container">
+								</div>
+							</div>
+								</div>
+							</div>
+								<div class="primary-nav-section">
+									<div class="container">
+										<ul class="nav primary clone-main-menu" id="mercado_main" data-menuname="Main menu" >
+											<li class="menu-item home-icon">
+												<a href="index.php" class="link-term mercado-item-title"><i class="fa fa-home" aria-hidden="true"></i></a>
+											</li>
+											<li class="menu-item">
+												<a href="about-us.php" class="link-term mercado-item-title">About Us</a>
+											</li>
+											<li class="menu-item">
+												<a href="contact-us.php" class="link-term mercado-item-title">Contact Us</a>
+											</li>																	
+										</ul>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
+			<div class="wrap-logo-top left-section">
+				<a href="index.php" class="link-to-home"><img src="assets/images/logo-top-1.png" alt="mercado"></a>	
 			</div>
 		</div>
-	</header>
-
-	<div class="wrap-logo-top left-section">
-	<p><br/></p>
-	<p><br/></p>
-	<p><br/></p>
-	<p><br/></p>
-		<a href="index.php" class="link-to-home"><img src="assets/images/logo-top-1.png" alt="mercado"></a>
-	</div>
 <!--END OF NAV BAR-->
 
 <!--START CONTENT-->
