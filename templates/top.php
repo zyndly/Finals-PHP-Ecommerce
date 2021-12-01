8c95249f9aca735576ed8d4fe4b30b9e3cb5b338
Zyndly <zyndlykentperlinbaja@gmail.com>
2021-12-01 22:23:02 +0800

    add top-nav-footer

---
+++ templates/top.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Maxwell Shop</title>
+		<link rel="stylesheet" href="css/bootstrap.min.css"/>
+		<script src="js/jquery2.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="main.js"></script>
+		<link rel="stylesheet" type="text/css" href="./style.css">
+
+		<link rel="shortcut icon" type="image/x-icon" href="assets/images/favicon.ico">
+		<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic,900,900italic&amp;subset=latin,latin-ext" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Open%20Sans:300,400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="./assets/css/animate.css">
+		<link rel="stylesheet" type="text/css" href="./assets/css/font-awesome.min.css">
+		<link rel="stylesheet" type="text/css" href="./assets/css/owl.carousel.min.css">
+		<link rel="stylesheet" type="text/css" href="./assets/css/chosen.min.css">
+		<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+		<link rel="stylesheet" type="text/css" href="./assets/css/color-01.css">
+		<link rel="stylesheet" type="text/css" href="./assets/css/policy.css">
+
+		<style></style>
+	</head>
+<body>
